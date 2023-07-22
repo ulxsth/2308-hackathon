b2cda2a1a2d181a44c3170bd9984cb364ee8af02
m1kqn <137014023+suzuki689@users.noreply.github.com>
2023-07-22 21:26:33 +0900

    topのhtml
    topのhtml書きました

---
+++ php/top.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="../stylesheets/top.css">
+        <title>トップ画面</title>
+    </head>
+    <body>
+        <h1>問題を探す</h1>
+
+        <div class="popularity-quizzes-container">
+        <h2>人気の問題</h2>
+        <ul>
+            <li>
+                <a href="quiz/answer.php?id=1">
+                    <span class="quiz-title-span">test1</span>
+                    <span class="quiz-posted_date-span">xxxx/xx/xx</span>
+                </a>
+            </li>
+        </ul>
+        </div>
+        <div class="recentry-quizzes-container">  
+        <h2>新しい問題</h2>
+        <ul>
+            <li>
+                <a href="quiz/answer.php?id=2">
+                    <span class="quiz-title-span">test2</span>
+                    <span class="quiz-posted_date-span">xxxx/xx/xx</span>
+                </a>
+            </li>
+        </ul>
+        </div>
+    </body>
+</html>

ab29f630f99444514d4ed72e0e824f3d6bf5207b
Meiki <mickey.0103.eiking@outlook.jp>
2023-07-23 22:23:43 +0900

    htmlを少し修正して、cssを追加しました

--- php/quiz/new.php
+++ php/quiz/new.php
@@ -2,32 +2,34 @@
 <html lang="ja">
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="/Thinker/stylesheets/new.css">
         <title>Thinker</title>
     </head>
     <body>
+        <div class="operation-screen">
         <form action="created.php" method="post">
         <h1>クイズ作成</h1>
-        <div class="quiz-name>
-            <label for="title">クイズ名</label>
-            <input type="text" name="title"><br>
+        <div class="quiz-name form-content">
+            <label for="title" class="quiz-mei">クイズ名</label><br>
+            <input type="text" name="title" class="quiz-text"><br>
         </div>
-        <div class="problem">
-            <label for="content">問題文</label>
-            <input type="text" name="content"><br>
+        <div class="problem form-content">
+            <label for="content" class="content-mei">問題文</label><br>
+            <input type="text" name="content" class="content-text"><br>
         </div>
         <div class="choice">
-            <label for="choices[]">選択肢</label>
+            <label for="choices[]">選択肢</label><br>
             1<input type="text" name="choices[]"><br>
             2<input type="text" name="choices[]"><br>
             3<input type="text" name="choices[]"><br>
             4<input type="text" name="choices[]"><br>
         </div>  
         <div class="correct">
-            <label for="answer">正解</label>
+            <label for="answer">正解</label><br>
             <input type="text" name="answer"><br>
         </div>
         <div class="problem-explanation">
-            <label for="explanation">解説</label>
+            <label for="explanation">解説</label><br>
             <textarea name="explanation" placeholder="問題に関する解説を入力してください"></textarea><br>
         </div>
         <input type="submit" value="確定" class="submit-button">

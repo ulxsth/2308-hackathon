0c745cbbdbe82660d74672867b0fe6c0318c1909
yotu <114195789+ulxsth@users.noreply.github.com>
2023-07-24 08:24:37 +0900

    divを追加

---
+++ php/quiz/result.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thinker</title>
+</head>
+<body>
+    <div class="contents-container">
+        <div class="result-container">
+            <div class="result">
+                <img src="/thinker/assets/images/correct.png">
+            </div>
+        </div>
+        <div class="recommend-container">
+            <div class="quizzes-same_auther"></div>
+            <div class="quizzes-recommend"></div>
+            <button onclick="location.href='#'">トップに戻る</button>
+        </div>
+    </div>
+</body>
+</html>

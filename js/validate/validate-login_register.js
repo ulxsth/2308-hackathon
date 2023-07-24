e0607a15efff8420981054f8c7e62a73060f3940
Gingira1213 <gin12132828@icloud.com>
2023-07-24 23:57:48 +0900

    jsをかいた

--- js/validate/validate-login_register.js
+++ js/validate/validate-login_register.js
@@ -1,18 +1,20 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
-function validateInput() {
-    var elem = document.getElementById("login-id-error");
-    // 数字のみの入力はfalse
-    if (/^\d+$/.test(elem)) {
-      return false;
-    }
+function validateAndDisplayId() {
+    var loginId = document.getElementById("login-id-error");
+    var loginPass = document.getElementById("login-pass-error");
+    var legisterId = document.getElementById("register-id-error");
+    var legisterPass = document.getElementById("register-pass-error");
+    var legisterPassConfirm = document.getElementById("register-pass-confirm-error");
     // 1文字以上15文字以下か
-    if (.test(elem))
+    if (loginId.length<1 || loginId.length>15){
+        //文字についてのエラーをここに
+    }
     // 許可する入力は英数字、アンダースコアだけ
-    if (/^[a-zA-Z0-9_]*$/.test(elem)) {
+    if (/^[a-zA-Z0-9_]*$/.test(loginId)) {
       // アンダースコアが先頭・末尾にある場合はfalse
-      if (/^[_]|[_]$/.test(elem)) {
+      if (/^[_]|[_]$/.test(loginId)) {
         return false;
       }
       // それ以外はtrue

8640817d40d995d3e01c889de1124fe08a6e1839
Meiki <mickey.0103.eiking@outlook.jp>
2023-07-24 20:11:42 +0900

    完成させました

--- php/quiz/new.php
+++ php/quiz/new.php
@@ -17,23 +17,31 @@
             <label for="content" class="content-mei">問題文</label><br>
             <input type="text" name="content" class="content-text"><br>
         </div>
-        <div class="choice">
-            <label for="choices[]">選択肢</label><br>
-            1<input type="text" name="choices[]"><br>
-            2<input type="text" name="choices[]"><br>
-            3<input type="text" name="choices[]"><br>
-            4<input type="text" name="choices[]"><br>
+        <div class="choice form-content">
+            <label for="choices[]" class="choice-mei">選択肢</label><br>
+            1<input type="text" name="choices[]" class="choice-text"><br>
+            2<input type="text" name="choices[]" class="choice-text"><br>
+            3<input type="text" name="choices[]" class="choice-text"><br>
+            4<input type="text" name="choices[]" class="choice-text"><br>
         </div>  
-        <div class="correct">
-            <label for="answer">正解</label><br>
-            <input type="text" name="answer"><br>
+        <div class="correct form-content">
+            <label for="answer" class="answer-mei">正解</label><br>
+            <select>
+            <option value="" selected hidden>選択してください</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            </select>
         </div>
-        <div class="problem-explanation">
-            <label for="explanation">解説</label><br>
-            <textarea name="explanation" placeholder="問題に関する解説を入力してください"></textarea><br>
+        <div class="problem-description form-content">
+            <label for="description" class="description-mei">解説</label><br>
+            <textarea name="description"  placeholder="問題に関する解説を入力してください" class="content-text"></textarea><br>
         </div>
+        <div class="button">
         <input type="submit" value="確定" class="submit-button">
         <input type="reset" value="リセット" class="reset-button">
+        </div>
         </form>
     </body>
 </html>

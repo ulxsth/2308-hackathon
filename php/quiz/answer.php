9f74d5e58ef6340ee7c5637a2be14641972ea061
Hiroshi7700 <137484523+Hiroshi7700@users.noreply.github.com>
2023-07-24 20:55:27 +0900

    cssで色をつけた
    cssで色をつけた
    cssでスペースなども広げた

--- php/quiz/answer.php
+++ php/quiz/answer.php
@@ -6,12 +6,17 @@
 		<title>answer</title>
 	</head>
 	<body>
+        <div class="operation_screen-quiz-body">
         <h1>問題</h1>
-        南アフリカ原産で、カフェインが含まれておらず、その甘い香りと赤い色か<br>
-        ら”赤いプッシュ”とも呼ばれている、紅茶の種類は？<br>
+        <p class="text">南アフリカ原産で、カフェインが含まれておらず、その甘い香りと赤い色か<br>
+        ら”赤いプッシュ”とも呼ばれている、紅茶の種類は？</p>
             <form action="top.php"method="post">
             <button type="submit" class="choice-button">ダージリンティー</button><br>
             <button type="submit" class="choice-button">アールグレイティー</button><br>
             <button type="submit" class="choice-button">アッサムティー</button><br>
             <button type="submit" class="choice-button">ルイボスティー</button><br>
+            </form>
+            </div>
+            </body>
+            </html>
 

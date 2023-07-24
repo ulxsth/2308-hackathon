8884d3a0e8903194654045ce3eda57c53b79368b
Hiroshi7700 <137484523+Hiroshi7700@users.noreply.github.com>
2023-07-24 22:05:23 +0900

    修正しました。
    <div>を追加した。
    修正した

--- php/quiz/answer.php
+++ php/quiz/answer.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html lang="ja">
-	<head>
-		<meta charset="UTF-8">
-		<link rel="stylesheet" href="/Thinker/stylesheets/quiz/answer.css">
-		<title>answer</title>
-	</head>
-	<body>
-        <div class="operation_screen-quiz-body">
-        <h1>問題</h1>
-        <p class="text">南アフリカ原産で、カフェインが含まれておらず、その甘い香りと赤い色か<br>
-        ら”赤いプッシュ”とも呼ばれている、紅茶の種類は？</p>
-            <form action="top.php"method="post">
-            <button type="submit" class="choice-button">ダージリンティー</button><br>
-            <button type="submit" class="choice-button">アールグレイティー</button><br>
-            <button type="submit" class="choice-button">アッサムティー</button><br>
-            <button type="submit" class="choice-button">ルイボスティー</button><br>
-            </form>
-            </div>
-            </body>
-            </html>
 
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/Thinker/stylesheets/quiz/answer.css">
+    <title>answer</title>
+</head>
+
+<body>
+    <div class="content-container">
+        <div class="operation_screen-quiz-body">
+            <h1>問題</h1>
+            <p class="text">南アフリカ原産で、カフェインが含まれておらず、その甘い香りと赤い色から”赤いプッシュ”とも呼ばれている、紅茶の種類は？</p>
+            <form action="top.php" method="post">
+                <button type="submit" class="choice-button">ダージリンティー</button><br>
+                <button type="submit" class="choice-button">アールグレイティー</button><br>
+                <button type="submit" class="choice-button">アッサムティー</button><br>
+                <button type="submit" class="choice-button">ルイボスティー</button><br>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>

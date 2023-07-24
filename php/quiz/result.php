583393f973f0585981cc85241af2df7b0d751bbe
yotu <114195789+ulxsth@users.noreply.github.com>
2023-07-24 09:33:39 +0900

    cssファイルを作成

--- php/quiz/result.php
+++ php/quiz/result.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Thinker</title>
+    <link rel="stylesheet" href="/thinker/stylesheets/result.css">
 </head>
 <body>
     <div class="contents-container">

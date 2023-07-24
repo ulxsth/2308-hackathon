0f4c8165e4b0aa4ef651657927faf98917746dfa
m1kqn <137014023+suzuki689@users.noreply.github.com>
2023-07-24 19:39:53 +0900

    html,css修正しました
    html,cssの修正をしました
    確認してください

--- php/top.php
+++ php/top.php
@@ -23,7 +23,7 @@
             <li>
                 <div class="quiz-item">
                     <a href="quiz/answer.php?id=2">
-                        <span class="quiz-title-span">test1</span>
+                        <span class="quiz-title-span">test2</span>
                         <span class="quiz-posted_date-span">xxxx/xx/xx</span>
                     </a>
                 </div>
@@ -31,7 +31,7 @@
             <li>
                 <div class="quiz-item">
                     <a href="quiz/answer.php?id=3">
-                        <span class="quiz-title-span">test1</span>
+                        <span class="quiz-title-span">test3</span>
                         <span class="quiz-posted_date-span">xxxx/xx/xx</span>
                     </a>
                 </div>
@@ -44,7 +44,7 @@
             <li>
                 <div class="quiz-item">
                     <a href="quiz/answer.php?id=4">
-                        <span class="quiz-title-span">test2</span>
+                        <span class="quiz-title-span">test4</span>
                         <span class="quiz-posted_date-span">xxxx/xx/xx</span>
                     </a>
                 </div>
@@ -52,7 +52,7 @@
             <li>
                 <div class="quiz-item">
                     <a href="quiz/answer.php?id=5">
-                        <span class="quiz-title-span">test2</span>
+                        <span class="quiz-title-span">test5</span>
                         <span class="quiz-posted_date-span">xxxx/xx/xx</span>
                     </a>
                 </div>
@@ -60,7 +60,7 @@
             <li>
                 <div class="quiz-item">
                     <a href="quiz/answer.php?id=6">
-                        <span class="quiz-title-span">test2</span>
+                        <span class="quiz-title-span">test6</span>
                         <span class="quiz-posted_date-span">xxxx/xx/xx</span>
                     </a>
                 </div>

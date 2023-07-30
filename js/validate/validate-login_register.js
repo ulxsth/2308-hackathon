5851b2951e85e2b648e69b3ea91562f9d257291b
Gingira1213 <gin12132828@icloud.com>
2023-07-30 21:38:52 +0900

    完成?

--- js/validate/validate-login_register.js
+++ js/validate/validate-login_register.js
@@ -1,42 +1,97 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
-function validateAndDisplay(id,pass) {
-    
-    var alphanumeric = /^[a-zA-Z0-9_]*$/;
-    var alphanumericsymbol = /^[a-zA-Z0-9!@#$%^&*]*$/;
-    var thatisall = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])$/;
-    // 1文字以上15文字以下か
-    if (/{1,15}/.test(id)){
-      document.write("1文字以上15文字以下にしてください")
+function handleForm(formType) {
+  const form = formType === 'registration' ? document.getElementById("registerForm") : document.getElementById("loginForm");
+  const errorMessagesDiv = document.getElementById("errorMessages");
+  errorMessagesDiv.innerHTML = "";
+
+  if (formType === 'login') {
+    const loginUserId = form.loginUserId.value;
+    const loginPassword = form.loginPassword.value;
+
+    // バリデーションのチェック
+    if (!validateUserId(loginUserId)) {
+        displayErrorMessage("ユーザIDは英数字と_（アンダーバー）のみで構成される必要があります。");
+        return;
     }
 
-    // 許可する入力は英数字、アンダースコアだけ
-    if (!alphanumeric.test(id)) {
-      document.write("英数字かアンダーバーだけにしてください")
-    }
-    
-    //8文字以上36文字以下か
-    if(/{8,36}/.test(pass)){
-      document.write("8文字以上36文字以下にしてください")
-    }
-
-    //許可するのは英数字、記号のみ
-    if (!alphanumericsymbol.test(pass)) {
-      document.write("英数字か記号のみにしてください。")
-    }
-
-    //大文字、小文字、数字をそれぞれ1文字以上含んでいるか
-    if(!thatisall.test(pass)){
-      document.write("大文字、小文字、数字をそれぞれ1文字以上いれてください。")
-    }
-
-    //パスワードの確認
-    var registerpass=document.getElementById("passward_register");
-    var registerpassconfirm=document.getElementById("passward_confirm");
-    if(registerpass != registerpassconfirm ){
-      document.write("入力値が一致しません。")
-    }
-
-
+    if (!validateUserIdCharacterLimit(loginUserId)) {
+      displayErrorMessage("ユーザIDは1文字以上15文字以下である必要があります。");
+      return;
   }
+
+    // ログイン処理（この部分を実際のログイン処理に置き換える）
+    alert("ログインが成功しました！");
+} else if (formType === 'registration') {
+      const userId = form.userId.value;
+      const password = form.password.value;
+      const confirmPassword = form.confirmPassword.value;
+
+      // バリデーションのチェック
+      if (!validateUserId(userId)) {
+          displayErrorMessage("ユーザIDは英数字と_（アンダーバー）のみで構成する必要があります。");
+          return;
+      }
+
+      if (!validateUserIdCharacterLimit(userId)) {
+        displayErrorMessage("1文字以上15文字以下である必要があります。");
+        return;
+    }
+
+      if (!validatePassword(password)) {
+        displayErrorMessage("パスワードは英数字、もしくは記号（!@#$%^&*）のみで構成される必要があります");
+        return;
+    }
+
+    if (!validatePasswordCharacterLimit(password)) {
+      displayErrorMessage("パスワードは,8文字以上36文字以下で構成する必要があります。");
+      return;
+  }
+
+  if (!validatePasswordCase(password)) {
+    displayErrorMessage("パスワードは大文字、小文字、数字をそれぞれ1文字以上含んでいる必要があります。");
+    return;
+}
+
+    if (password !== confirmPassword) {
+        displayErrorMessage("確認用パスワードが一致しません。");
+        return;
+    }
+
+    // 新規登録処理（この部分を実際の新規登録処理に置き換える）
+    alert("新規登録が成功しました！");
+    }
+}
+
+function validateUserId(userId) {
+  const userIdRegex = /^[a-zA-Z0-9_]$/;
+  return userIdRegex.test(userId);
+}
+
+function validateUserIdCharacterLimit(userId) {
+  const userIdCharacterLimitRegex = /^{1,15}$/;
+  return userIdCharacterLimitRegex.test(userId);
+}
+
+function validatePassword(password) {
+  const passwordRegex = /^[a-zA-Z\d!@#$%^&*]$/;
+  return passwordRegex.test(password);
+}
+
+function validatePasswordCharacterLimit(password) {
+  const passwordCharacterLimitRegex = /^{8,36}$/;
+  return passwordCharacterLimitRegex.test(password);
+}
+
+function validatePasswordCase(password) {
+  const passwordCaseRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)$/;
+  return passwordCaseRegex.test(password);
+}
+
+function displayErrorMessage(message) {
+  const errorMessageDiv = document.createElement("div");
+  errorMessageDiv.innerText = message;
+  errorMessageDiv.style.color = "red";
+  document.getElementById("errorMessages").appendChild(errorMessageDiv);
+}
--- php/user/login_register.php
+++ php/user/login_register.php
@@ -6,29 +6,20 @@
     <link rel="stylesheet" href="/Thinker/stylesheets/user/login_register.css">
     <title>ログイン・新規登録画面</title>
 </head>
-<script src="/Thinker/js/validate/validate-login_register.js">    
 </script>
 <body>
     <div class="contents-container">
         <h1>ログイン</h1>
         <div class="login">
-            <form action="login.php" method="POST">
+            <form action="login.php" onsubmit="return handleForm('login');" method="POST" id="loginForm">
                 <div class="form-component">
                     <label for="id" class="form-label">ユーザーID</label>
-                    <input type="text" name="id" class="form-input" required>
-                    <div id="login-id-error" class="error">
-                        エラー文
-
-                    </div>
+                    <input type="text" id ="login_id" name="login_id" class="form-input" required>
                 </div>
                 <br>
                 <div class="form-component">
                     <label for="passward" class="form-label">パスワード</label>
-                    <input type="text" name="passward_login" class="form-input" required>
-                    <div id="login-pass-error" class="error">
-                        エラー文
-
-                    </div>
+                    <input type="text" id ="login_pass" name="login_pass" class="form-input" required>
                 </div>
                 <button type="submit" class="button-input">ログイン</button>
             </form>
@@ -38,36 +29,26 @@
 
         <h1>新規登録</h1>
         <div class="register">
-            <form action="login.php" method="POST">
+            <form action="login.php" onsubmit="return handleForm('registration');" method="POST" id="registerFrom">
                 <div class="form-component">
                     <label for="id" class="form-label">ユーザーID</label>
-                    <input type="text" name="id" class="form-input" required>
-                    <div id="register-id-error" class="error">
-                        エラー文
-
-                    </div>
+                    <input type="text" id ="register_id" name="register_id" class="form-input" required>
                 </div>
                 <br>
                 <div class="form-component">
                     <label for="passward" class="form-label">パスワード</label>
-                    <input type="text" name="passward_logister" class="form-input" required>
-                    <div id="register-pass-error" class="error">
-                        エラー文
-
-                    </div>
+                    <input type="text" id ="register_pass" name="register_pass" class="form-input" required>
                 </div>
                 <br>
                 <div class="form-component">
                     <label for="passward_confirm" class="form-label">パスワード(確認)</label>
-                    <input type="text" name="passward_confirm" class="form-input" required>
-                    <div id="register-pass-confirm-error" class="error">
-                        エラー文
-
-                    </div>
+                    <input type="text" id ="passconfirm name="register_passward_confirm" class="form-input" required>
                 </div>
                 <button type="submit" class="button-input">新規登録</button>
-            </form>
         </div>
+        </form>
+        <div id="errorMessages"></div>
+        <script src="/Thinker/js/validate/validate-login_register.js"></script>
 </body>
 
 </html>

601a319615a32b0788f1fd2a0d885be12fae3747
Hiroshi7700 <137484523+Hiroshi7700@users.noreply.github.com>
2023-08-05 21:30:00 +0900

    ある程度できた気がします
    タブでやったけどうまくいきません

--- app/view/profile.php
+++ app/view/profile.php
@@ -14,93 +14,109 @@
             <p class="text">紅茶検定2級です。紅茶の問題を出します。</p>
             <form action="top.php" method="post">
 
-    つくった問題　　解いた問題</br>
+            <div class="sample-area">
+                <p id="tabcontrol">
+                    <div class="quiz-cliate">
+                        <a href="#tabpage1">つくった問題</a>
+                        </div>
+                    <div class="quiz-solved">
+                        <a href="#tabpage2">解いた問題</a>
+                    </div>
+                </p>
+            <div id="tabbody">
 
-    <li>
-        <div class="quiz-item">
-            <a href="quiz/answer.php?id=1">
-                <span class="quiz-title-span">紅茶の酸化</span>
-                <span class="quiz-posted_date-span">xxxx/xx/xx</span>
-            </a>
-        </div>
-    </li>
-    <li>
-        <div class="quiz-item">
-            <a href="quiz/answer.php?id=2">
-                <span class="quiz-title-span">イギリスの伝統的な紅茶</span>
-                <span class="quiz-posted_date-span">xxxx/xx/xx</span>
-            </a>
-        </div>
-    </li>
-    <li>
-        <div class="quiz-item">
-            <a href="quiz/answer.php?id=3">
-                <span class="quiz-title-span">カフェインを含まない紅茶</span>
-                <span class="quiz-posted_date-span">xxxx/xx/xx</span>
-            </a>
-        </div>
-    </li>
-    <li>
-        <div class="quiz-item">
-            <a href="quiz/answer.php?id=3">
-                <span class="quiz-title-span"></span>
-                <span class="quiz-posted_date-span">xxxx/xx/xx</span>
-            </a>
-        </div>
-    </li>
-    <li>
-        <div class="quiz-item">
-            <a href="quiz/answer.php?id=3">
-                <span class="quiz-title-span"></span>
-                <span class="quiz-posted_date-span">xxxx/xx/xx</span>
-            </a>
-        </div>
-    </li>
-    <li>
-        <div class="quiz-item">
-            <a href="quiz/answer.php?id=3">
-                <span class="quiz-title-span"></span>
-                <span class="quiz-posted_date-span">xxxx/xx/xx</span>
-            </a>
-        </div>
-    </li>
-    <li>
-        <div class="quiz-item">
-            <a href="quiz/answer.php?id=3">
-                <span class="quiz-title-span"></span>
-                <span class="quiz-posted_date-span">xxxx/xx/xx</span>
-            </a>
-        </div>
-    </li>
-    <li>
-        <div class="quiz-item">
-            <a href="quiz/answer.php?id=3">
-                <span class="quiz-title-span"></span>
-                <span class="quiz-posted_date-span">xxxx/xx/xx</span>
-            </a>
-        </div>
-    </li>
-    <li>
-        <div class="quiz-item">
-            <a href="quiz/answer.php?id=3">
-                <span class="quiz-title-span"></span>
-                <span class="quiz-posted_date-span">xxxx/xx/xx</span>
-            </a>
-        </div>
-    </li>
-    <li>
-        <div class="quiz-item">
-            <a href="quiz/answer.php?id=3">
-                <span class="quiz-title-span"></span>
-                <span class="quiz-posted_date-span">xxxx/xx/xx</span>
-            </a>
-        </div>
-    </li>
+            <div id="tabpage1">
+            <div id="tabpage2">
+
+    <ul>
+        <li>
+            <div class="quiz-item">
+                <a href="quiz/answer.php?id=1">
+                    <span class="quiz-title-span">紅茶の酸化</span>
+                    <span class="quiz-posted_date-span">xxxx/xx/xx</span>
+                </a>
+            </div>
+        </li>
+        <li>
+            <div class="quiz-item">
+                <a href="quiz/answer.php?id=2">
+                    <span class="quiz-title-span">イギリスの伝統的な紅茶</span>
+                    <span class="quiz-posted_date-span">xxxx/xx/xx</span>
+                </a>
+            </div>
+        </li>
+        <li>
+            <div class="quiz-item">
+                <a href="quiz/answer.php?id=3">
+                    <span class="quiz-title-span">カフェインを含まない紅茶</span>
+                    <span class="quiz-posted_date-span">xxxx/xx/xx</span>
+                </a>
+            </div>
+        </li>
+        <li>
+            <div class="quiz-item">
+                <a href="quiz/answer.php?id=3">
+                    <span class="quiz-title-span"></span>
+                    <span class="quiz-posted_date-span">xxxx/xx/xx</span>
+                </a>
+            </div>
+        </li>
+        <li>
+            <div class="quiz-item">
+                <a href="quiz/answer.php?id=3">
+                    <span class="quiz-title-span"></span>
+                    <span class="quiz-posted_date-span">xxxx/xx/xx</span>
+                </a>
+            </div>
+        </li>
+        <li>
+            <div class="quiz-item">
+                <a href="quiz/answer.php?id=3">
+                    <span class="quiz-title-span"></span>
+                    <span class="quiz-posted_date-span">xxxx/xx/xx</span>
+                </a>
+            </div>
+        </li>
+        <li>
+            <div class="quiz-item">
+                <a href="quiz/answer.php?id=3">
+                    <span class="quiz-title-span"></span>
+                    <span class="quiz-posted_date-span">xxxx/xx/xx</span>
+                </a>
+            </div>
+        </li>
+        <li>
+            <div class="quiz-item">
+                <a href="quiz/answer.php?id=3">
+                    <span class="quiz-title-span"></span>
+                    <span class="quiz-posted_date-span">xxxx/xx/xx</span>
+                </a>
+            </div>
+        </li>
+        <li>
+            <div class="quiz-item">
+                <a href="quiz/answer.php?id=3">
+                    <span class="quiz-title-span"></span>
+                    <span class="quiz-posted_date-span">xxxx/xx/xx</span>
+                </a>
+            </div>
+        </li>
+        <li>
+            <div class="quiz-item">
+                <a href="quiz/answer.php?id=3">
+                    <span class="quiz-title-span"></span>
+                    <span class="quiz-posted_date-span">xxxx/xx/xx</span>
+                </a>
+            </div>
+        </li>
+    </ul>
 
 
             </form>
         </div>
     </div>
+</div>
+</div>
 </body>
 
 </html>

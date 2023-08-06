330770d3516723d788792e4c59ba3466d7d8ad67
yotu <114195789+ulxsth@users.noreply.github.com>
2023-08-06 11:26:46 +0900

    add: SessionとuserIdの紐づけを行うためのサービスクラスを作成

---
+++ app/service/SessionService.php
@@ -0,0 +1,17 @@
+<?php
+class SessionService {
+    public static $sessionMap = array();
+
+    public static function set($sessionId, $userId) {
+        self::$sessionMap[$sessionId] = $userId;
+    }
+
+    public static function get($sessionId) {
+        return self::$sessionMap[$sessionId];
+    }
+
+    public static function getAllUser() {
+        return array_values(self::$sessionMap);
+    }
+}
+?>

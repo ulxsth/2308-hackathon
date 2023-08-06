292e6c1fe73f3cb11a6ef521fb2b6622e3ba9eaf
yotu <114195789+ulxsth@users.noreply.github.com>
2023-08-06 11:27:05 +0900

    update: signinを実装

--- app/service/UserService.php
+++ app/service/UserService.php
@@ -18,5 +18,33 @@ class UserService {
         // ユーザ登録
         return $dao->insert($name, $password_hash);
     }
+
+    public static function signin($name, $password) {
+        $dao = new UserDAO();
+
+        // ユーザ名で検索
+        $user = $dao->findByName($name);
+        if (is_null($user)) {
+            throw new Exception("$name is not found.");
+        }
+
+        // パスワードの照合
+        if (!password_verify($password, $user->getPasswordHash())) {
+            throw new Exception("Password is invalid.");
+        }
+
+        // すでにセッション上に存在していないか確認
+        $users = SessionService::getAllUser();
+        if(in_array($user->getId(), $users)) {
+            throw new Exception("$name is already signed in.");
+        }
+
+        // セッションにユーザIDを保存
+        session_start();
+        SessionService::set(session_id(), $user->getId());
+
+        // ログイン後の画面にリダイレクト
+        header('Location: http://' . $_SERVER['HTTP_HOST'] . '/thinker/app/view/top.php');
+    }
 }
 ?>

f394728fa35db68218f8abc2a532de905b83a6e2
yotu <114195789+ulxsth@users.noreply.github.com>
2023-08-06 12:00:18 +0900

    fix: 画面遷移はaction内で行うように変更

--- app/controller/UserController.php
+++ app/controller/UserController.php
@@ -16,9 +16,6 @@ class UserController {
 
         // ユーザ登録
         UserService::signup($name, $password_hash);
-
-        // トップにリダイレクト
-        header('Location: http://' . $_SERVER['HTTP_HOST'] . '/thinker/app/view/top.php');
     }
 
     public static function signin($name, $password) {

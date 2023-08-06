1ef9c4fd8a8bd75a8d5110a55305d5245f3733fb
yotu <114195789+ulxsth@users.noreply.github.com>
2023-08-06 12:16:15 +0900

    fix: 二重hash問題を修正

--- app/service/UserService.php
+++ app/service/UserService.php
@@ -5,19 +5,21 @@ include dirname(__FILE__) . '/../dao/UserDAO.php';
  * UserDAOとUserControllerの接続をするクラス
  */
 class UserService {
-    public static function signup($name, $password_hash) {
+    public static function signup($name, $password) {
         $dao = new UserDAO();
+
         // ユーザ名の重複チェック
         if (!is_null($dao->findByName($name))) {
             throw new Exception("$name is already exists.");
         }
 
         // パスワードのハッシュ化
-        $password_hash = password_hash($password_hash, PASSWORD_DEFAULT);
+        $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
         // ユーザ登録
         return $dao->insert($name, $password_hash);
     }
+
 
     public static function signin($name, $password) {
         $dao = new UserDAO();

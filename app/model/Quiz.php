a7cb0b2a78fff45cd3eb8fab3389e243fd2f7576
yotu <114195789+ulxsth@users.noreply.github.com>
2023-08-06 09:33:03 +0900

    update: CRUDのうちCRの関数を実装

--- app/model/Quiz.php
+++ app/model/Quiz.php
@@ -1,3 +1,43 @@
 <?php
+class Quiz {
+    private $id;
+    private $title;
+    private $content;
+    private $description;
+    private $author_id;
+    private $created_at;
 
+    public function __construct($id, $title, $content, $description, $author_id, $created_at) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->content = $content;
+        $this->description = $description;
+        $this->author_id = $author_id;
+        $this->created_at = $created_at;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getContent() {
+        return $this->content;
+    }
+
+    public function getDescription() {
+        return $this->description;
+    }
+
+    public function getAuthorId() {
+        return $this->author_id;
+    }
+
+    public function getCreatedAt() {
+        return $this->created_at;
+    }
+}
 ?>
